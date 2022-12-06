ace977685e058590a7e3ce2181677fcdc1e92755
yanJuicy <hanjub3@gmail.com>
2022-12-06 22:26:15 +0900

    chapter03 엔터티

---
+++ src/main/java/jpastart/reserve/model/Review.java
@@ -0,0 +1,60 @@
+package jpastart.reserve.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "hotel_review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hotel_id")
+    private String hotelId;
+
+    private int rate;
+
+    private String comment;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "rating_date")
+    private Date ratingDate;
+
+    protected Review() {}
+
+    public Review(String hotelId, int rate, String comment, Date ratingDate) {
+        this.hotelId = hotelId;
+        this.rate = rate;
+        this.comment = comment;
+        this.ratingDate = ratingDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getRatingDate() {
+        return ratingDate;
+    }
+}
